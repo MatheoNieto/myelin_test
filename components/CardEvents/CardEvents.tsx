29320b1d2b70b0e2b764650401c555edfdd155d8
Mateo Nieto <matheo.developer@gmail.com>
2024-12-01 21:31:30 +0000

    card events

--- components/CardEvents/CardEvents.tsx
+++ components/CardEvents/CardEvents.tsx
@@ -1,26 +1,70 @@
-import {Box} from '@/ui/components';
-import {Image} from 'react-native';
+import {Box, Text} from '@/ui/components';
+import {Image, StyleSheet} from 'react-native';
 import React from 'react';
 
 type Props = {
   urlImage: string;
+  title: string;
 };
-const CardEvents: React.FC<Props> = ({urlImage}) => {
+const CardEvents: React.FC<Props> = ({urlImage, title}) => {
   return (
-    <Box p="m" backgroundColor="white">
+    <Box mr="xs" my="s">
       <Image
         source={{
           uri: urlImage,
         }}
-        width={130}
-        height={130}
+        width={160}
+        height={160}
         resizeMode="cover"
         style={{
           borderRadius: 5,
         }}
       />
+
+      <Box
+        width={160}
+        height={160}
+        borderRadius="s"
+        style={{
+          position: 'absolute',
+          top: 0,
+          right: 0,
+          backgroundColor: 'rgba(0, 0,0,0.5)',
+        }}>
+        <Box p="s" top={100} height={60}>
+          <Text mb="xs" color="white">
+            {title}
+          </Text>
+          <Box
+            flexDirection="row"
+            alignItems="center"
+            justifyContent="space-between">
+            <Box flexDirection="row">
+              <Box style={styles.labels} px="s" borderRadius="s">
+                <Text color="white">2</Text>
+              </Box>
+
+              <Box style={styles.labels} px="s" mx="xs" borderRadius="s">
+                <Text color="white">2</Text>
+              </Box>
+            </Box>
+
+            <Box>
+              <Box style={styles.labels} px="s" mx="xs" borderRadius="s">
+                <Text color="white">2</Text>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
     </Box>
   );
 };
+
+const styles = StyleSheet.create({
+  labels: {
+    backgroundColor: 'rgba(255,255,255, 0.3)',
+  },
+});
 
 export default CardEvents;
--- containers/ExpiresEvents.tsx
+++ containers/ExpiresEvents.tsx
@@ -9,18 +9,32 @@ const ExpiresEvents = () => {
     {
       urlImage:
         'https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbCUyMHdhbGxwYXBlcnxlbnwwfHwwfHx8MA%3D%3D',
+      title: 'Farmhouse Plan',
+    },
+    {
+      urlImage:
+        'https://c4.wallpaperflare.com/wallpaper/172/921/248/nature-water-sea-travel-wallpaper-preview.jpg',
+      title: 'Farmhouse Plan',
+    },
+    {
+      urlImage:
+        'https://yorkshirefoodguide.co.uk/wp-content/uploads/2019/06/Ox-Club-Leeds-Chop-Night-.jpg',
+      title: 'Farmhouse Plan',
+    },
+    {
+      urlImage:
+        'https://cdn-3.expansion.mx/dims4/default/2ab5265/2147483647/resize/1280x/quality/90/?url=https%3A%2F%2Fcdn-3.expansion.mx%2F3f%2F58%2Fe40c85894fb291192144ea18ca63%2Faeromexico-viajes.jpg',
+      title: 'Farmhouse Plan',
     },
   ];
 
   const renderItems = ({item}: {item: {urlImage: string}}) => (
-    <CardEvents urlImage={item.urlImage} />
+    <CardEvents title="Farmhouse Plan" urlImage={item.urlImage} />
   );
 
   return (
-    <Box>
-      <Box mx="m">
-        <Pointer type="expires" title="Expires in 5 days!" />
-      </Box>
+    <Box px="m">
+      <Pointer type="expires" title="Expires in 5 days!" />
       <FlatList
         horizontal
         data={data}
