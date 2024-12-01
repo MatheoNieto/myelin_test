dc67ad0992f42612dc6b66c94b5845ef51e7f9d7
Mateo Nieto <matheo.developer@gmail.com>
2024-12-01 16:31:42 +0000

    carousel

--- components/CardPlans/MiniatureCarousel.tsx
+++ components/CardPlans/MiniatureCarousel.tsx
@@ -1,51 +1,66 @@
+import {Box} from '@/ui/components';
 import React from 'react';
-import {View, Text, Image, StyleSheet, Dimensions} from 'react-native';
+import {Image, StyleSheet} from 'react-native';
 import Carousel from 'react-native-snap-carousel';
-
-const {width} = Dimensions.get('window');
 
 const MiniatureCarousel = () => {
   const carouselItems = [
-    {id: 1, image: 'https://via.placeholder.com/30', title: 'Image 1'},
-    {id: 2, image: 'https://via.placeholder.com/30', title: 'Image 2'},
-    {id: 3, image: 'https://via.placeholder.com/30', title: 'Image 3'},
-    {id: 4, image: 'https://via.placeholder.com/30', title: 'Image 4'},
+    {
+      id: 1,
+      image:
+        'https://uncle.co.uk/app/uploads/2024/04/UNCLE-LEEDS_Area_FINAL-183.jpg',
+    },
+    {
+      id: 2,
+      image:
+        'https://youmedia-cdn.s3.eu-west-2.amazonaws.com/wp-content/uploads/2024/02/28110212/leeds-skyline_402434702-as.jpg',
+    },
+    {
+      id: 3,
+      image:
+        'https://pic.uhomes.com/onlineblog/cdn/uploads/2024/02/leeds-city-centre.jpg',
+    },
+    {
+      id: 4,
+      image:
+        'https://static.leonardo-hotels.com/image/Leeds_2048x1367_desktop_2.jpeg',
+    },
   ];
 
   const renderItem = ({item}: {item: any}) => {
-    return <Image source={{uri: item.image}} style={styles.image} />;
+    return (
+      <Box
+        width={70}
+        mr="s"
+        height={70}
+        p="xxs"
+        borderWidth={1}
+        borderColor="grayIcon"
+        borderRadius="s">
+        <Image source={{uri: item.image}} style={styles.image} />
+      </Box>
+    );
   };
 
   return (
-    <View style={styles.container}>
+    <Box>
       <Carousel
         layout="stack"
-        layoutCardOffset={18}
         data={carouselItems}
         renderItem={renderItem}
-        sliderWidth={width}
-        itemWidth={100}
+        sliderWidth={100}
+        itemWidth={500}
       />
-    </View>
+    </Box>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
   image: {
-    width: 50,
-    height: 50,
-    resizeMode: 'contain',
-  },
-  title: {
-    marginTop: 10,
-    fontSize: 14,
-    color: '#333',
+    width: 65,
+    height: 65,
+    resizeMode: 'cover',
+    borderRadius: 5,
   },
 });
 
